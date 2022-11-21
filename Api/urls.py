5a9eee005d5bc074593b6171b11a1f5467da180e
kasemsh99 <kasemshaker@hotmail.com>
2022-11-21 10:37:35 +0330

    0

--- Api/urls.py
+++ Api/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
 	path('artist/<int:artist_id>/', views.artist_data),
     path('artist/<int:artist_id>/edit/', views.artist_edit),
     path('artist/search/', views.artist_search),
-	path('media/search/', views.media_search),
+	
 ]
