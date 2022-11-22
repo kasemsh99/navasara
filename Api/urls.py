031dddfd68f9e167f2d1e5881f1739c2a5b4eec3
Mujtabamirzaeii <116367088+Mujtabamirzaeii@users.noreply.github.com>
2022-11-22 22:22:39 +0330

    urls

--- Api/urls.py
+++ Api/urls.py
@@ -3,10 +3,14 @@ from . import views
 
 urlpatterns = [
     path('login/', views.user_login),
+    path('register/', views.user_register),
 
 
 	path('artist/<int:artist_id>/', views.artist_data),
     path('artist/<int:artist_id>/edit/', views.artist_edit),
     path('artist/search/', views.artist_search),
 	
+    path('media/search/', views.media_search),
+    path('media/create/', views.media_create),
+    path('media/<int:media_id>/like/add/', views.add_like),
 ]
